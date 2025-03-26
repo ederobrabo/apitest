2b358444f727a333c3bf91434d3ee46736cd2fe1
ederobrabo <ederede2003@gmail.com>
2025-03-26 11:05:15 -0300

    fixed PostRepository

--- src/main/java/com/eder/apitest/repositories/PostRepository.java
+++ src/main/java/com/eder/apitest/repositories/PostRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
-    public Post findByTitle(String title);
 }
