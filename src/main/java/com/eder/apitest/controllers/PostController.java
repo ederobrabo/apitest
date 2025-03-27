f2b27d647248deda01f79c8d92ba7a2ad0df170f
ederobrabo <ederede2003@gmail.com>
2025-03-26 21:02:19 -0300

    created controllers

---
+++ src/main/java/com/eder/apitest/controllers/PostController.java
@@ -0,0 +1,49 @@
+package com.eder.apitest.controllers;
+
+import com.eder.apitest.domain.post.Post;
+import com.eder.apitest.domain.post.PostDTO;
+import com.eder.apitest.domain.post.PostUpdateDTO;
+import com.eder.apitest.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPost(@PathVariable UUID id) throws Exception {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> savePost(PostDTO data) {
+        return ResponseEntity.ok(postService.createPost(data));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable UUID id, PostUpdateDTO data) throws Exception {
+        return ResponseEntity.ok(postService.updatePost(id, data));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable UUID id) {
+        try {
+            postService.deletePost(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
---
+++ src/main/java/com/eder/apitest/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.eder.apitest.controllers;
+
+import com.eder.apitest.domain.user.User;
+import com.eder.apitest.domain.user.UserDTO;
+import com.eder.apitest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable UUID id) throws Exception {
+        return ResponseEntity.ok(userService.getUser(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> postUser(UserDTO data) {
+        return ResponseEntity.ok(userService.createUser(data));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable UUID id, UserDTO data) throws Exception {
+        return ResponseEntity.ok(userService.updateUser(id, data));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        try {
+            userService.deleteUser(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
